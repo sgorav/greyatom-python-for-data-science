ad9e27f3d39f11f622ac0450bb2dba1b9c86ab39
sgorav <sgorav@users.noreply.github.com>
2020-05-10 00:51:09 +0530

    Code file created.

---
+++ project/code.py
@@ -0,0 +1,79 @@
+# --------------
+# Code starts here
+
+class_1 = ['Geoffrey','Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2 = ['Hilary Mason','Carla Gentry','Corinna Cortes']# Create the lists 
+
+new_class = class_1 + class_2# Concatenate both the strings
+print(new_class)
+new_class.append('Peter Warden')
+print(new_class)
+new_class.remove('Carla Gentry')
+print(new_class)
+# Append the list
+
+# Print updated list
+
+
+# Remove the element from the list
+
+# Print the list
+
+
+
+courses = {'Math':65, 'English':70, 'History':80, 'French':70, 'Science':60}
+print(courses)
+total = courses['Math'] + courses['English'] + courses['History'] + courses['French'] + courses['Science']
+print(total)
+percentage = total *100/500
+print(percentage)# Create the Dictionary
+
+
+
+# Slice the dict and stores  the all subjects marks in variable
+
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+
+# Insert percentage formula
+
+# Print the percentage
+
+
+
+
+mathematics = {'Geoffrey Hinton':78, 'Andrew Ng':95, 'Sebastian Raschka':65,'Yoshua Benjio':50, 'Hilary Mason':70, 'Corinna Cortes':66, 'Peter Warden':75}
+max_marks_scored = max(courses,key = courses.get)
+print (max_marks_scored)# Create the Dictionary
+ 
+topper = max(mathematics,key = mathematics.get)
+print(topper)
+
+# Given string
+
+
+def convert(lst):
+    return (lst[0].split())
+lst = ["Andrew Ng"]
+print(convert(lst))
+first_name = convert(lst)[0]
+print(first_name)
+second_name = convert(lst)[1]
+print(second_name)
+full_name = second_name + ' ' + first_name
+
+# To split the words of a sentence
+# Create variable Last_name and store last two element in the list
+
+certificate_name = full_name.upper()
+print(certificate_name)# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
